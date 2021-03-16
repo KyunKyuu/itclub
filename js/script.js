b091a26542e15530e3a0361dbcf3ace77d94217e
Rizkan Firmansyah <riezkanaprianda@gmail.com>
2021-03-17 04:32:52 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+(function($) {
+    $(function() {
+
+        $('.sidenav').sidenav();
+
+        $('.parallax').parallax();
+
+        $('.carousel').carousel().next(3);
+
+    }); // end of document ready
+})(jQuery);
