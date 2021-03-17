f01867368afa3ac168b56915e88bd995046b4c27
Rizkan Firmansyah <riezkanaprianda@gmail.com>
2021-03-17 08:03:13 +0700

    templating dashboard, monolith website

---
+++ resources/views/include/dashboard/header.blade.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Dashboard &mdash; ITClub</title>
+
+  {{--!NOTE memanggil assets css dashboard  --}}
+  @include('assets.dashboard.css')
+
+</head>
---
+++ resources/views/templates/dashboard.blade.php
@@ -0,0 +1,9 @@
+@include('include.dashboard.header')
+
+@include('include.dashboard.navbar')
+
+@include('include.dashboard.sidebar')
+
+@yield('main')
+
+@include('include.dashboard.footer')
