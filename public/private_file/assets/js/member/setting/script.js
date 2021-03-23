607cc6081e4fe87972fd711799cc9d9af7b7c15d
Rizkan Firmansyah <riezkanaprianda@gmail.com>
2021-03-23 15:42:29 +0700

    update change password

--- public/private_file/assets/js/member/setting/script.js
+++ public/private_file/assets/js/member/setting/script.js
@@ -7,4 +7,12 @@ $(document).ready(function() {
         SeePassword({idPassword : 'oldPassword', idButton : 'seeOldPassword'});
     })
 
+    $('#changePassword').on('submit', function(e) {
+        e.preventDefault();
+        if ($('#changePassword input[name="oldpassword"]').val() == '' || $('#changePassword input[name="newpassword"]').val() == '') {
+            $('#alert').html(` <div class="alert alert-danger alert-dismissible show fade"><div class="alert-body"><button class="close" data-dismiss="alert"><span>×</span></button>Isi dulu atuhhh.</div></div>`)
+            return 0;
+        }
+    })
+
 })
