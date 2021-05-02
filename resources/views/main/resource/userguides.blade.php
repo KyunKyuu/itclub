ef7110f31fb42f3c3a4ec55730e478f3f88c38c0
Rizkan Firmansyah <riezkanaprianda@gmail.com>
2021-05-03 04:19:48 +0700

    update

--- resources/views/main/resource/userguides.blade.php
+++ resources/views/main/resource/userguides.blade.php
@@ -7,8 +7,17 @@
         <div class="row">
             <div class="col-12">
                 <div class="card p-4">
-                    <div id="Step">
-                        <span>Unknown</span>
+                    <div id="Step" data-id="{{ $_GET['uid'] }}">
+                        <div class="card-body">
+                            <h3 class="text-center">{{ $_GET['slug'] }}</h3>
+                            <hr>
+                            <div class="list-unstyled list-unstyled-border mt-4" id="detail">
+                                <div class="media">
+
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
